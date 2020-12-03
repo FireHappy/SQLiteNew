284a907c781a5a2dadea563f6e6b19cd40283e26
chenlin <chenlin@pinming.com>
2020-12-03 09:16:06 +0800

    Revert "嘿嘿"
    This reverts commit d04235b54825160c422f8534338e3d2845849389.

---
+++ Assets/SQLite/Scripts/SQLiteTest.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections.Generic;
+using Assets.SQLite.Scripts.Manager;
+using Assets.SQLite.Scripts.Model;
+using Assets.SQLite.Scripts.Repository;
+using UnityEngine;
+
+namespace Assets.SQLite.Scripts
+{
+    /// <summary>
+    /// SQLite的demo
+    /// </summary>
+    public class SQLiteTest : MonoBehaviour
+    {
+        private BaseRepository<User> repository = null;
+
+        private void Awake()
+        {
+            repository = SQLiteManager.Instance.GetRepository<User>();            
+        }
+
+        private void Start()
+        {           
+            //DeleteAll();
+            //InsertTest();
+            //InsertList();
+            //UpdateTest();
+            //DeleteTest();
+            //SelectTest(); 
+            SelectListTest();
+            //SelectAllTest();
+        }
+
+        private void DeleteAll()
+        {
+            repository.DeleteAll();
+        }
+
+        private void InsertTest()
+        {
+            User user = new User
+            {
+                Id = 1,
+                UserName = "test",
+                Age = 10,
+                PassWord = 123456789
+            };     
+            repository.Insert(user);
+        }
+
+
+        private void InsertList()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                User user = new User
+                {
+                    Id = i,
+                    UserName = null,
+                    Age = 10,
+                    PassWord = 123456789
+                };
+                repository.Insert(user);
+            }
+        }
+
+       
+
+        private void DeleteTest()
+        {
+            repository.DeleteByPrimaryKey(1);
+        }
+
+        private void UpdateTest()
+        {
+            User user = new User
+            {
+                Id = 1,
+                UserName = "test--update" ,
+                Age = 10,
+                PassWord = 123456789
+            };
+            repository.UpdateByPrimaryKey(user);
+        }
+
+        private void SelectTest()
+        {
+            Dictionary<string, object> user = repository.SelectByPrimaryKey(2);            
+        }
+
+        private void SelectListTest()
+        {
+            List<Dictionary<string, object>> userList = repository.SelectListByColumn("大帅哥", "UserName");
+            Debug.Log("user.count:"+userList.Count);
+        }
+
+        private void SelectAllTest()
+        {
+            List<Dictionary<string, object>> userList = repository.SelectAll();
+            Debug.Log("user.count:" + userList.Count);
+        }
+    }
+}
